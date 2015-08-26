f0f284e683a3121b3859bdd56c2cb0e85e669521
Robin Berjon <robin@berjon.com>
2015-08-26 18:27:48 +0200

    update now supported correctly

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 var sua = require("superagent")
+,   fs = require("fs-extra")
+,   pth = require("path")
+,   exec = require("child_process").exec
 ;
 
 // here is what we want
@@ -27,6 +30,42 @@ exports.loadConfiguration = function (path) {
   return conf;
 }
 
+function ensureDir (dir) {
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+}
+
+function fetchRepo (conf, repo) {
+  var repoDir = pth.join(conf.dataDir, repo);
+  if (!fs.existsSync(repoDir)) return cloneRepo(conf, repo);
+  exec("git fetch", { cwd: repoDir }, function (err) {
+    if (err) return console.error("Error fetching " + repo + " inside " + repoDir + ": " + err);
+    console.log("Updated " + repo + " in " + repoDir);
+  });
+}
+
+function cloneRepo (conf, repo) {
+  var repoDir = pth.join(conf.dataDir, repo);
+  if (fs.existsSync(repoDir)) return fetchRepo(conf, repo);
+  exec("git clone --mirror https://github.com/w3c/" + repo + ".git " + repoDir, function (err) {
+    if (err) return console.error("Error cloning repository: " + repo + ", " + err);
+    console.log("Cloned " + repo + " to " + repoDir);
+  });
+}
+
+// set up all known public repositories
 exports.init = function (conf) {
+  if (!conf.dataDir) throw new Error("Missing configuration field: dataDir.");
+  ensureDir(conf.dataDir);
+  sua.get("https://api.github.com/orgs/w3c/repos?type=public")
+      .accept("json")
+      .end(function (err, res) {
+        if (err) return console.error("Error setting up: " + err);
+        res.body
+            .map(function (it) { return it.name; })
+            .forEach(function (it) { cloneRepo(conf, it); })
+      })
+}
+
+exports.update = function (conf) {
 
 }
